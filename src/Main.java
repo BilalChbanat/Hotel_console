9b515be4f63cd3669c9a592a75b80b30f314bd51
BilalChbanat <bilal.chbanat2003@gmail.com>
2024-08-30 16:17:15 +0100

    making the menu for the users with switch case and do while

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,6 +13,48 @@ public class Main {
         rooms.add(r2);
         rooms.add(r3);
         System.out.println(rooms.get(1).getAvailability());
+        System.out.println(
+                "╔══════════════════════════════════════════════════════════════════════════════════════╗\n" +
+                "║                                                                                      ║\n" +
+                "║                                   Hotel Reservation System                           ║\n" +
+                "║                                                                                      ║\n" +
+                "╚══════════════════════════════════════════════════════════════════════════════════════╝");
+        int input;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("║\t1: Display all available rooms  ║");
+            System.out.println("║\t2: Make a reservation\t\t\t║");
+            System.out.println("║\t3: Update a reservation\t\t\t║");
+            System.out.println("║\t4: Cancel a reservation \t\t║");
+            System.out.println("║\t5: Exit\t\t\t\t\t\t\t║");
+            System.out.print("\t Enter your choice: \t");
+
+            input = sc.nextInt();
+
+            switch (input) {
+                case 1:
+                    System.out.println("all romms");
+                    break;
+                case 2:
+                    System.out.println("create a reservation");
+                    break;
+                case 3:
+                    System.out.println("update reservation");
+                    break;
+                case 4:
+                    System.out.println("remove a reservation");
+                    break;
+                case 5:
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 5.");
+            }
+
+        } while (input != 5);
+
+        sc.close();
+
 
     }
 }
